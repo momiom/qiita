af868c8d80b02b4efb03dbc58f5cd7d5aedc2405
河﨑 一希 <kawasaka@creo.co.jp>
2019-03-26 18:31:09 +0900

    get_items date処理追加

--- main.py
+++ main.py
@@ -29,21 +29,22 @@ def get_tags():
 
 
 def get_items(yyyymm, page=1, per_page=100):
-    print('fetching yyyymm:{} page:{}'.format(yyyymm, page), file=sys.stderr)
-    r = c.list_items(
-        params={
-            'page': page,
-            'per_page': per_page,
-            'query': 'created:{}'.format(yyyymm),
-        })
-    return r
+    print('fetching yyyymm:{} page:{}'.format(yyyymm, page))
+    uri = '/api/v2/items'
+    params = {
+        'page': page,
+        'per_page': per_page,
+        'query': 'created:{}'.format(yyyymm)
+    }
+    response = requests.get(url=HOST+uri, params=params)
+    return json_loads(response.text)
 
 
-start = datetime.strptime('2018-01', '%Y/%m')
+start = datetime.strptime('2018-01', '%Y-%m')
 end = datetime.strptime('2019-03', '%Y-%m')
+
 for date in daterange(start, end):
+    get_items(date.strftime('%Y-%m'))
 
-
-tags = get_tags()
-with open('./tags.json', mode='w', encoding='utf-8') as f:
-    json.dump(tags, f, ensure_ascii=False, indent=4)
+# with open('./tags.json', mode='w', encoding='utf-8') as f:
+#     json.dump(tags, f, ensure_ascii=False, indent=4)
