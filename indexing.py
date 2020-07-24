e3703403ba6ae75a57d3a3b67679a6551deb965f
K-Kazuki <kazuki.k.bl8@gmail.com>
2020-07-24 09:25:28 +0900

    elasticsearchへのindexing用スクリプト ループ内でbulkを呼ぶように実装

--- indexing.py
+++ indexing.py
@@ -15,7 +15,7 @@ def create_index(index, mapping, setting):
 
 def load_data():
     monthly_data_list = []
-    for d in glob('/Users/kazuki/Develop/python_projects/qiita/data/**/*.json', recursive=True):
+    for d in glob('./data/**/*.json', recursive=True):
         with open(d) as f:
             monthly_data_list.append(json.load(f))
 
@@ -30,34 +30,59 @@ def load_data():
         print()
 
 
-host = 'localhost:9200'
-es = Elasticsearch(host)
+def load_data2():
+    monthly_data_list = []
+    for d in glob('./data/**/*.json', recursive=True):
+        with open(d, encoding='utf-8') as f:
+            monthly_data_list.append(json.load(f))
 
-index_name = 'qiita'
+    insert_data_list = []
+    for monthly_data in monthly_data_list:
+        for d in monthly_data:
+            insert_data_list.append(
+                {
+                    '_index': 'qiita',
+                    '_type': '_doc',
+                    '_source': d
+                }
+            )
+            if len(insert_data_list) >= 100:
+                yield insert_data_list
+                insert_data_list = []
+                time.sleep(30)
 
-with open('./elasticsearch/setting.json') as f:
-    print('Load settings.')
-    setting = json.load(f)
-with open('./elasticsearch/mapping.json') as f:
-    print('Load mappings.')
-    mapping = json.load(f)
+    if len(insert_data_list) > 0:
+        yield insert_data_list
 
-if es.indices.exists(index=index_name):
-    print('Index exists. Deleted.')
-    es.indices.delete(index_name)
 
-print('Create index.')
-create_index(index_name, mapping, setting)
+def main():
+    global es
+    host = 'localhost:9200'
+    es = Elasticsearch(host)
+    index_name = 'qiita'
+    with open('./elasticsearch/setting.json') as f:
+        print('Load settings.')
+        setting = json.load(f)
+    with open('./elasticsearch/mapping.json') as f:
+        print('Load mappings.')
+        mapping = json.load(f)
+    if es.indices.exists(index=index_name):
+        print('Index exists. Deleted.')
+        es.indices.delete(index_name)
+    print('Create index.')
+    create_index(index_name, mapping, setting)
+    print(es.indices.get(index_name))
+    # bulkインサート
+    print('Insert start.')
+    t0 = time.time()
+    # helpers.bulk(es, actions=load_data(), chunk_size=1000, timeout=100)
+    for data in load_data2():
+        helpers.bulk(es, actions=data, request_timeout=100)
+    t1 = time.time()
+    elapsed_time = float(t1 - t0)
+    print('Done.')
+    print(elapsed_time)
 
-print(es.indices.get(index_name))
 
-# bulkインサート
-print('Insert start.')
-
-t0 = time.time()
-helpers.bulk(es, actions=load_data(), chunk_size=1000)
-t1 = time.time()
-elapsed_time = float(t1 - t0)
-
-print('Done.')
-print(elapsed_time)
+if __name__ == '__main__':
+    main()
