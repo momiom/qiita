304ad7f809defe19d697da8c275011d5ab4f66a4
K-Kazuki <kazuki.k.bl8@gmail.com>
2020-07-28 08:17:47 +0900

    一部の変数を環境変数化

--- settings.py
+++ settings.py
@@ -5,5 +5,9 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-slack_webhook_url = os.environ.get("slack_webhook_url")
-api_token = os.environ.get("api_token")
+slack_webhook_url = os.environ.get('slack_webhook_url')
+slack_user_id = os.environ.get('slack_user_id')
+api_token = os.environ.get('api_token')
+doc_data_dir = os.environ.get('doc_data_dir')
+sentences_data_dir = os.environ.get('sentences_data_dir')
+sentences_data_name = os.environ.get('sentences_data_name')
